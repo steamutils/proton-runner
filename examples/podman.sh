cee08c952df6ef0eb2a8ad0f2ed077a0f8912b1e
Chris Kuperstein <chris@kuperstein.net>
2024-04-19 10:39:01 -0700

    updating examples

--- examples/podman.sh
+++ examples/podman.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-podman run -d --name my-application \
-        -v steamapp-123456:/steamapp:Z \
-        -v proton-123456:/proton:Z \
+podman volume exists proton-2278520 || podman volume create proton-2278520
+podman volume exists steamapp-2278520 || podman volume create steamapp-2278520
+
+chmod 775 ./enshrouded_server.json
+
+podman run -d --name enshrouded \
+	-e STEAMCMD_ARGS='+@sSteamCmdForcePlatformType windows +force_install_dir /steamapp +login anonymous +app_update 2278520 -public validate +quit' \
+        -v steamapp-2278520:/steamapp:z \
+        -v proton-2278520:/proton:z \
+	-v ./enshrouded_server.json:/steamapp/enshrouded_server.json:z \
 	-p 15636:15636/udp \
 	-p 15637:15637/udp \
 	quay.io/steamutils/proton-runner:latest \
-	/steamapp/path/to/application.exe
+	enshrouded_server.exe
